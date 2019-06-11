8be8484319d8212aabcfcaa40502f9b0db41a81e
wjt <wangjitao.sir@gmail.com>
2019-06-11 15:32:42 +0800

    fix2

--- WJTCategoryKit/Classes/Utility/UIViewController/UIViewController+HUD.h
+++ WJTCategoryKit/Classes/Utility/UIViewController/UIViewController+HUD.h
@@ -7,13 +7,13 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "MBProgressHUD.h"
+//#import <MBProgressHUD/MBProgressHUD.h>
 
 @interface UIViewController (HUD)
 //显示菊花，如不取消一直显示
 - (void)hudShow;
 ///显示自定义loading样式,默认背景是无框透明，返回hud可继续进行定制
-- (MBProgressHUD *)hudShowWithCustomView:(UIView *)customView;
+//- (MBProgressHUD *)hudShowWithCustomView:(UIView *)customView;
 //隐藏 HUD
 - (void)hudHidden;
 
